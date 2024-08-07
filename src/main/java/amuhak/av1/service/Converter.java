a97ec874e998c8be310b47416f2aa92d1dc08156
Amulya Jain <amuly.jain@gmail.com>
2024-08-07 17:13:37 -0400

    Adjustable thread pool size

--- src/main/java/amuhak/av1/service/Converter.java
+++ src/main/java/amuhak/av1/service/Converter.java
@@ -1,13 +1,37 @@
 package amuhak.av1.service;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+@Service
 public class Converter {
-    static ThreadPoolExecutor executor = new ThreadPoolExecutor(1, 1, 5, TimeUnit.MINUTES, new LinkedBlockingDeque<>());
+
+    private static final Logger logger = org.slf4j.LoggerFactory.getLogger(Converter.class);
+
+    @Value("${thread.pool.size}")
+    private int maximumPoolSize;
+
+    private static ThreadPoolExecutor executor;
+
+    @PostConstruct
+    private void initializeExecutor() {
+        if (executor == null) {
+            executor = new ThreadPoolExecutor(1, maximumPoolSize, 5, TimeUnit.MINUTES, new LinkedBlockingDeque<>());
+            logger.info("Thread Pool has max size of: {}", maximumPoolSize);
+        }
+    }
 
     public static void addJob(Job job) {
-        executor.execute(job);
+        if (executor != null) {
+            executor.execute(job);
+        } else {
+            logger.error("Executor not initialized. Cannot add job.");
+        }
     }
 }
